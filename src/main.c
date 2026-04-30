022356acb6c6f25c22614bd799eed90192d18165
ezra-jaesu <259886796+ezra-jaesu@users.noreply.github.com>
2026-04-30 14:23:46 +0800

    Feat: Implement basic input and exit function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
-
+#include <string.h>
 int main() {
     printf("DMEP has been initialized!\n");
+
+    char command[256];
+
+    while(1) {
+	printf("> ");
+	fflush(stdout);
+	fgets(command, sizeof(command), stdin);
+
+	if (strcmp(command, "quit\n") == 0) {
+	    printf("\nQuitting...\n");
+	    return 0;
+	}
+
+    };
+
+
+
     return 0;
 }
